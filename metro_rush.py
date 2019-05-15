2692754cbda5b8d5318bc01a6703d0d0e437f260
nson85 <sonthien17@gmail.com>
2019-05-15 17:27:24 +0700

    6th

--- metro_rush.py
+++ metro_rush.py
@@ -4,22 +4,48 @@ from sys import stderr
 from re import match
 
 
-
-
 class Graph:
 
     def __init__(self, metrolines, start, end):
-        self.nodes = set_nodes(metrolines)  # {line:{station:node}}
+        self.nodes = set_nodes(metrolines) # {name:{line:id}}
         ################
         self.start_node = self.get_start_end_nodes(start)
         self.end_node = self.get_start_end_nodes(end)
         ################
-    ################
+
+
     def get_start_end_nodes(self, node):
         for key in self.nodes:
             for sub_key in self.nodes[key].station_id:
                 if sub_key == node[0] and self.nodes[key].station_id[sub_key] == node[1]:
-                    return {key:self.nodes[key]}
+                    return self.nodes[key]
+
+
+    def abc(self, metrolines):
+        for key in self.nodes:
+            if self.nodes[key].conn_pts:
+                self.find_neigbours(self.nodes[key], metrolines)
+
+
+    def find_neigbours(self, node, metrolines):
+        neigbour = []
+        print(node.station_name)
+        for line in metrolines:
+            tempo = []
+            for station in metrolines[line]:
+                if ":Conn:" in station and line in node.station_id.keys() and station not in tempo:
+                    tempo.append(station)
+                    for index, j in enumerate(tempo):
+                        try:
+                            if node.station_name in j and index != 0 and j not in neigbour:
+                                neigbour.append(tempo[index - 1])
+                                neigbour.append(tempo[index + 1])
+                            elif node.station_name in j and index == 0 and j not in neigbour:
+                                neigbour.append(tempo[index + 1])
+                        except:
+                            pass
+        node.neigbours = list(set(neigbour))
+
     ################
     def find_way():
         way = None
@@ -27,12 +53,16 @@ class Graph:
 
 class Node:
 
-    def __init__(self, station_name, station_id, conn_pts=False):
+    def __init__(self, station_name, station_id, metrolines, conn_pts=False):
         self.station_name = station_name
         # a dict contains tuples which are station ids of station
         # exp: {(line1: id1), (line2: id2)}
         self.station_id = station_id
         self.conn_pts = conn_pts
+        self.neigbours = None
+
+
+
 
 
 def get_metrolines(lines):
@@ -94,7 +124,6 @@ def get_file_name():
     return args.file_name
 
 
-
 def set_nodes(metrolines):
     node_dict = {}
     for line in metrolines:
@@ -107,11 +136,26 @@ def set_nodes(metrolines):
                 id, station_name = station.split(':')
                 conn_pts = False
             if station_name not in node_dict:
-                node_dict[station_name] = Node(station_name, {line: id}, conn_pts)
+                node_dict[station_name] = Node(station_name,
+                                               {line: id}, metrolines, conn_pts)
             else:
                 node_dict[station_name].station_id[line] = id
     return node_dict
 
+
+def find_neigbours_conn_pts(src_node, nodes_list):
+    print(src_node.station_id.keys())
+    neigbours_list = []
+    for node in nodes_list:
+        if node.conn_pts is True and get_edge(src_node, node) is not None:
+            neigbours_list += [node]
+    return neigbours_list
+
+
+def get_edge(src_conn_node, dest_conn_node):
+    for line in src_conn_node.station_id:
+        if line in dest_conn_node.station_id:
+            return abs(int(src_conn_node.station_id[line]) - int(dest_conn_node.station_id[line]))
 
 def main():
     file_name = get_file_name()
@@ -119,10 +163,18 @@ def main():
     start, end, trains_number = get_data(lines)
     metrolines = get_metrolines(lines)
     ################
-    graph = Graph(metrolines, start, end
+    graph = Graph(metrolines, start, end)
     #################
-    print(graph.start_node)
-    print(graph.end_node)
+    # print(graph.start_node)
+    # print(graph.end_node)
+    station1 = graph.nodes['Kashmere Gate']
+    station2 = graph.nodes['Mandi House']
+    # print(get_edge(station1, station2))
+    print(metrolines)
+    print(graph.nodes['Central Secretariat'].neigbours)
+
+    #print([node.station_name for node in find_neigbours_conn_pts(station1, graph.nodes.values())])
+    # print(graph.get_edge())
     #print(graph.get_start_end_nodes(start, end))
 
     # print((node_dict))
