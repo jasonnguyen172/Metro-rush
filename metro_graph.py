e1f25e03af4101a2216cf652d2683a7ef3d731c3
khagkhangg <48564724+khagkhangg@users.noreply.github.com>
2019-05-22 17:52:48 +0700

    Add comments

--- metro_graph.py
+++ metro_graph.py
@@ -3,7 +3,10 @@ from functions import set_nodes, find_name_of_station
 
 
 class Graph:
-
+    """
+    Store all informations of the metro
+    Including all station's instance
+    """
     def __init__(self, metrolines, start, end):
         self.nodes_dict = set_nodes(metrolines)  # {name:node_obj}
         self.start_node = self.get_start_end_nodes(metrolines, start)
@@ -23,27 +26,43 @@ class Graph:
         self.nodes_dict[station_name].connected = True
         return self.nodes_dict[station_name]
 
-    ########################
+    ############################################################################
+    #                                                                          #
+    #            Create attribute 'neihgbours' for node's instance             #
+    #                                                                          #
+    ############################################################################
     def filter_connected_points(self, metrolines):
+        """
+        Control the finding
+        """
         for line_name in self.nodes_dict:
             if self.nodes_dict[line_name].connected:
+                # find neihgbours of a connected-station
                 self.find_neihgbours(self.nodes_dict[line_name], metrolines)
 
     def find_neihgbours(self, node, metrolines):
+        """
+        Find all other nearest connected-stations of a station
+        Then store the result to station's attribute 'neihgbours'
+        """
         neihgbours = []
         for line in node.station_id:
             temporary_list = []
             for station in metrolines[line]:
+                # check if the station is connected
                 if "Conn" in station or station.split(':')[1] in\
                  [self.start_node.station_name, self.end_node.station_name]:
                     temporary_list.append(station)
             for index, element in enumerate(temporary_list):
                 try:
+                    # Add forward and backward stations
                     if node.station_name in element and index != 0:
                         neihgbours.append(self.nodes_dict[temporary_list[index - 1].split(':')[1]])
                         neihgbours.append(self.nodes_dict[temporary_list[index + 1].split(':')[1]])
+                    # Only add forward stations
                     elif node.station_name in element and index == 0:
                         neihgbours.append(self.nodes_dict[temporary_list[index + 1].split(':')[1]])
-                except:
+                except IndexError:
                     pass
+        # change the attribute neihgbours of this station's object
         node.neihgbours = neihgbours
