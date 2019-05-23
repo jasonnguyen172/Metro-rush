24ea13e5ad090349b2da031320a34087e3f77420
khagkhangg <48564724+khagkhangg@users.noreply.github.com>
2019-05-23 19:35:56 +0700

    stable version

--- station_nodes.py
+++ station_nodes.py
@@ -1,27 +1,18 @@
 class Node:
-    """
-    Store all informations of one station
-    """
+
     def __init__(self, station_name, station_id, connected=False):
         self.station_name = station_name
         # station_id: a dict with key= line and value= ID
         # exp: {line1: id1, line2: id2}
         self.station_id = station_id
         self.connected = connected
-        # a list, which store all neihgbour stations of this station
-        self.neihgbours = None
-        # a list, which store all trains that are on this station
+        self.neihgbours = []
         self.trains = []
 
 
 class Trains:
-    """
-    Store all information of one train
-    """
-    def __init__(self, current_line, start_node, train_name, stay=False):
+
+    def __init__(self, graph, station_node, train_name, stay=   False):
         self.train_name = train_name
-        # default = False, turn True if train need to stop,
         self.stay = stay
-        self.current_line = current_line
-        # add train instance to start node
-        start_node.trains.append(self)
+        station_node.trains.append(self)
